9dc4453e5e22225ab2823388a6c0f6ce5fbbce92
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 10:58:20 +0100

    explanation works

--- src/components/ApiNasa.tsx
+++ src/components/ApiNasa.tsx
@@ -5,13 +5,16 @@ const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
 
 export const ApiNasa = () => {
 	const [date, setDate] = useState('');
+	const [explanation, setExplanation] = useState('');
+	const [imageUrl, setImageUrl] = useState('');
 
 	const handleGetPhoto = (e:React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault();
 		(async () => {
 			const response = await axios.get(url);
-			// const data = response.data;
-			console.log(response);
+			const data = response.data;
+			const _explanation = data.explanation;
+			setExplanation(_explanation);
 		})();
 	}
 
@@ -20,7 +23,7 @@ export const ApiNasa = () => {
 			<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
 				<input value={date} onChange={(e) => setDate(e.target.value)} />
 				<button onClick={(e) => handleGetPhoto(e)}>Get photo of the day</button>
-				<p>(photo and explanation)</p>
+				<p>{explanation}</p>
 			</div>
 		</form>
 	)
