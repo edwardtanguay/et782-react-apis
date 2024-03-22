e5392d2d128e6423357fad6f647bb5bc588a9f51
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 10:33:31 +0100

    fetch doesn't work

--- src/components/ApiNasa.tsx
+++ src/components/ApiNasa.tsx
@@ -1,17 +1,24 @@
-import { useState } from "react"
+import axios from "axios";
+import { useState } from "react";
+
+const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
 
 export const ApiNasa = () => {
 	const [date, setDate] = useState('');
 
 	const handleGetPhoto = () => {
-		alert(date);
+		(async () => {
+			const response = await axios.get(url);
+			// const data = response.data;
+			console.log(response);
+		})();
 	}
 
 	return (
 		<form>
 			<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
 				<input value={date} onChange={(e) => setDate(e.target.value)} />
-				<button onClick={(e) => handleGetPhoto(e)}>Get photo of the day</button>
+				<button onClick={handleGetPhoto}>Get photo of the day</button>
 				<p>(photo and explanation)</p>
 			</div>
 		</form>
