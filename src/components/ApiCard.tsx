0e5bfed879cd702b3afcd21a046dd0ff4ea5f020
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 11:42:46 +0100

    cardwidth works

--- src/components/ApiCard.tsx
+++ src/components/ApiCard.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 export const ApiCard = ({ children, cardWidth = '50rem' }: IProps) => {
 	return (
-		<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
+		<div style={{width: cardWidth}} className="flex gap-5 flex-col bg-slate-200 p-6 rounded">
 			<h2>cardWidth: {cardWidth}</h2>
 			{children}
 		</div>
