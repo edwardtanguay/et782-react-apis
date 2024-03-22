4a1b59ca8c5470cfcd4b24e3f7d03cae0ae92cf0
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 09:46:52 +0100

    can type word

--- src/components/ApiDictionary.tsx
+++ src/components/ApiDictionary.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react"
+
 export const ApiDictionary = () => {
+	const [word, setWord] = useState('house');
+
 	return (
 		<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
-			<input/>
+			<input value={word} onChange={(e) => setWord(e.target.value)} />
 			<button>Get definition</button>
 			<p>(definition)</p>
 		</div>
