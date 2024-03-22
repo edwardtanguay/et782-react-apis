43d40b6a2a97101f24d1583c84b8cb87d039dc3a
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 10:18:15 +0100

    dictionary api finished

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -2,8 +2,8 @@ import { ApiDictionary } from "../components/ApiDictionary"
 
 export const PageWelcome = () => {
 	return (
-		<>
+		<div className="flex gap-3 flex-wrap">
 		<ApiDictionary/>
-		</>
+		</div>
 	)
 }
