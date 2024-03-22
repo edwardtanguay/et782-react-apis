e6b57d6e0fd33361927cf790e6bace70326459e7
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 09:56:03 +0100

    button clicks alert

--- src/components/ApiDictionary.tsx
+++ src/components/ApiDictionary.tsx
@@ -1,19 +1,22 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 const url = 'https://api.dictionaryapi.dev/api/v2/entries/en';
 
 export const ApiDictionary = () => {
 	const [word, setWord] = useState('house');
 
-	const handleGetDefinition = () => {
+	const handleGetDefinition = (e:React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
 		alert('clicked')
 	}
 
 	return (
-		<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
-			<input value={word} onChange={(e) => setWord(e.target.value)} />
-			<button onClick={handleGetDefinition}>Get definition</button>
-			<p>(definition)</p>
-		</div>
+		<form>
+			<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
+				<input value={word} onChange={(e) => setWord(e.target.value)} />
+				<button onClick={(e) => handleGetDefinition(e)}>Get definition</button>
+				<p>(definition)</p>
+			</div>
+		</form>
 	)
 }
