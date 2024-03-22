a049baa4381bb1248d85d53f7dec2e64da9d29b9
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 10:49:01 +0100

    fixed reload page

--- src/components/ApiNasa.tsx
+++ src/components/ApiNasa.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
 
 export const ApiNasa = () => {
 	const [date, setDate] = useState('');
 
-	const handleGetPhoto = () => {
+	const handleGetPhoto = (e:React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
 		(async () => {
 			const response = await axios.get(url);
 			// const data = response.data;
@@ -18,7 +19,7 @@ export const ApiNasa = () => {
 		<form>
 			<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
 				<input value={date} onChange={(e) => setDate(e.target.value)} />
-				<button onClick={handleGetPhoto}>Get photo of the day</button>
+				<button onClick={(e) => handleGetPhoto(e)}>Get photo of the day</button>
 				<p>(photo and explanation)</p>
 			</div>
 		</form>
