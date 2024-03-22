5393aff5c74db381cb24e33dcf294653b876f865
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 09:42:34 +0100

    basic card finished

--- src/components/ApiDictionary.tsx
+++ src/components/ApiDictionary.tsx
@@ -1,5 +1,9 @@
 export const ApiDictionary = () => {
 	return (
-		<p>dictionary</p>
+		<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
+			<input/>
+			<button>Get definition</button>
+			<p>(definition)</p>
+		</div>
 	)
 }
